d691d877470fe1bd7b4382062fd4c4eacfbff47f
quanzhen <quanzhen.huang@italki.com>
2022-01-05 06:04:52 +0000

    消息内容超出部分的省略显示

--- lib/pages/sub_home_page.dart
+++ lib/pages/sub_home_page.dart
@@ -13,7 +13,6 @@ import 'package:frisbee/utils/event_util.dart';
 import 'package:frisbee/utils/request_util.dart';
 import 'package:flutter_svg/svg.dart';
 
-
 class SubHomePage extends StatefulWidget {
   SubHomePage({Key? key, required this.icon, required this.header})
       : super(key: key);
@@ -40,11 +39,9 @@ class _SubHomePageState extends State<SubHomePage>
   Future _getMessageData() async {
     if (Global.teams.isNotEmpty) {
       var teamId = Global.teams[0]!.id;
-      var params = {
-        'limit': '5',
-        'sort': 'desc'
-      };
-      var data = await ApiClient().get('/api/teams/$teamId/messages', params: params);
+      var params = {'limit': '5', 'sort': 'desc'};
+      var data =
+          await ApiClient().get('/api/teams/$teamId/messages', params: params);
       return data;
     }
   }
@@ -117,22 +114,42 @@ class _SubHomePageState extends State<SubHomePage>
 
   List<Widget> _buildMessages(messages) {
     List<Widget> list = [];
-    //i<5, pass your dynamic limit as per your requirment 
+    //i<5, pass your dynamic limit as per your requirment
     for (var message in messages) {
-      var messageRow = Row(children: [
-      message['userPicture'] == null
-                              ? SvgPicture.asset("assets/player.svg", width: 20,)
-                              : CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                    message['userPicture'],
-                                  ),
-                                  radius: 10,
-                                ),
-                              const SizedBox(width: 5,),
-        Text(message['userName'], style: const TextStyle(fontSize: 10),),
-        const SizedBox(width: 10,),
-        Text(message['text'], style: const TextStyle(fontSize: 10),maxLines: 1, overflow: TextOverflow.ellipsis,),
-      ],);
+      var messageRow = Row(
+        children: [
+          message['userPicture'] == null
+              ? SvgPicture.asset(
+                  "assets/player.svg",
+                  width: 20,
+                )
+              : CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    message['userPicture'],
+                  ),
+                  radius: 10,
+                ),
+          const SizedBox(
+            width: 5,
+          ),
+          Text(
+            message['userName'],
+            style: const TextStyle(fontSize: 13),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Flexible(
+              child: Container(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Text(
+              message['text'],
+              style: const TextStyle(fontSize: 13),
+              overflow: TextOverflow.ellipsis,
+            ),
+          )),
+        ],
+      );
       list.add(messageRow);
     }
     return list;
@@ -244,8 +261,14 @@ class _SubHomePageState extends State<SubHomePage>
             ),
           ),
           Card(
-            child: Container(margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20), height: 130, child: Column(children: _buildMessages(messages), mainAxisAlignment: MainAxisAlignment.spaceAround,),)  
-          )
+              child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            height: 130,
+            child: Column(
+              children: _buildMessages(messages),
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            ),
+          ))
         ],
       ),
     );
@@ -290,4 +313,3 @@ class _SubHomePageState extends State<SubHomePage>
     ));
   }
 }
-
