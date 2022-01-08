ce5c8bff9961e453a9a127b951c9379bc45522fc
montehuang <hqzmonte@sina.com>
2022-01-08 02:26:48 +0000

    战术详情和讨论数据的获取

--- lib/pages/book_details.dart
+++ lib/pages/book_details.dart
@@ -5,6 +5,9 @@
  */
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:frisbee/utils/request_util.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:frisbee/common/global.dart';
 
 class BookDetailPage extends StatefulWidget {
   BookDetailPage({Key? key, required this.book}) : super(key: key);
@@ -12,26 +15,44 @@ class BookDetailPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // ignore: no_logic_in_create_state
-    return _BookDetailPageState(book: book);
+    return _BookDetailPageState();
   }
 }
 
 class _BookDetailPageState extends State<BookDetailPage>
     with TickerProviderStateMixin, RestorationMixin {
-  _BookDetailPageState({required this.book});
-  Map book;
+  _BookDetailPageState();
   late TabController _controller;
   final RestorableInt _tabIndex = RestorableInt(0);
 
+  var _futureGetMessage;
+  var _futureGetBook;
   @override
   void initState() {
     super.initState();
+    _futureGetMessage = _getMessageData();
+    _futureGetBook = _getBookData();
     _controller = TabController(length: 2, vsync: this, initialIndex: 0);
     _controller.addListener(() {
       setState(() {
         _tabIndex.value = _controller.index;
       });
     });
+  }
+
+  Future _getMessageData() async {
+    if (widget.book['team'] != null) {
+      var data = await ApiClient().get('/api/teams/${widget.book['team']}/messages');
+      return data;
+    }
+  }
+
+  Future _getBookData() async {
+    if (widget.book['_id'] != null && widget.book['team'] != null) {
+      var data = await ApiClient()
+          .get('/api/teams/${widget.book['team']}/games/${widget.book['_id']}');
+      return data;
+    }
   }
 
   @override
@@ -42,7 +63,9 @@ class _BookDetailPageState extends State<BookDetailPage>
         automaticallyImplyLeading: false,
         toolbarHeight: 30,
         titleSpacing: 0,
-        title: Text(book['name'],),
+        title: Text(
+          widget.book['name'],
+        ),
         bottom: TabBar(
           // padding: EdgeInsets.symmetric(horizontal: 20),
           controller: _controller,
@@ -57,7 +80,44 @@ class _BookDetailPageState extends State<BookDetailPage>
       body: TabBarView(
         controller: _controller,
         children: [
-          for (var tab in tabs) Container(margin:EdgeInsets.symmetric(vertical: 5.r), child: Text(tab), color: Colors.redAccent,)
+          FutureBuilder(
+              future: _futureGetBook,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return const Center(
+                      child: Text('获取战术出错'),
+                    );
+                  } else {
+                    return const Center(
+                      child: Text('获取战术成功'),
+                    );
+                  }
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+          FutureBuilder(
+              future: _futureGetMessage,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return const Center(
+                      child: Text('获取消息出错'),
+                    );
+                  } else {
+                    return const Center(
+                      child: Text('获取消息成功'),
+                    );
+                  }
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              })
         ],
       ),
     );
